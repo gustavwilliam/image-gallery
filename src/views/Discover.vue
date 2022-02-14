eeff85b4f9c0dce75878d027a0e5cd16b187cecf
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-02-14 16:50:38 +0100

    Add header to discover and random page

--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -1,12 +1,22 @@
 <template>
   <TheMainContentFrame>
+    <div class="px-1 pb-10">
+      <h1
+        class="leading-6 my-2 font-bold tracking-tight text-gray-900 dark:text-gray-100 text-2xl"
+      >
+        Discover images
+      </h1>
+      <p class="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-400">
+        Browse all the available categories below.
+      </p>
+    </div>
     <GridFeatured :categories="categories" />
   </TheMainContentFrame>
 </template>
 
 <script lang="ts">
 import GridFeatured from "@/components/Grids/GridFeatured/GridFeatured.vue";
-import TheMainContentFrame from "@/components/TheMainContent.vue";
+import TheMainContentFrame from "@/components/TheMainContentFrame.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <TheMainContentFrame>
+    <div class="px-1 pb-10">
+      <h1
+        class="leading-6 my-2 font-bold tracking-tight text-gray-900 dark:text-gray-100 text-2xl"
+      >
+        Random image
+      </h1>
+      <p class="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-400">
+        Here's a random image from the collections:
+      </p>
+    </div>
+  </TheMainContentFrame>
 </template>
 
 <script>
+import TheMainContentFrame from "@/components/TheMainContentFrame";
 export default {
+  components: { TheMainContentFrame },
   props: {
     categories: { string: {} },
   },
